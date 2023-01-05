9debcc12e37a25de7fafdae56715e6a3430b962f
adpsu <64772508+adpsu@users.noreply.github.com>
2023-01-04 19:51:14 -0500

    Update pems_submit.sh

--- notebooks/pems_submit.sh
+++ notebooks/pems_submit.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-dist = 4
-year = 2020
-#SBATCH --job-name=PEMS_JOB_DIST_$dist_$year
+#SBATCH --job-name=PEMS_JOB_DIST_4_2021
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=amd7293@psu.edu
 #SBATCH --nodes=1
@@ -9,17 +7,17 @@ year = 2020
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64gb
 #SBATCH --time=01:00:00
-#SBATCH --output=PEMS_JOB_DIST_$dist_$year.log
+#SBATCH --output=PEMS_JOB_DIST_4_2021.log
 cd $SLURM_SUBMIT_DIR
 source ~/.bashrc
 module purge
 module load anaconda3
 conda activate pems
-python3 pems_data_downloader.py --user_id 'amd7293@psu.edu' --password 'A09381g@0027' --district $dist --start_year $year --end_year $year --file_type 'station_5min'
+python3 pems_data_downloader.py --user_id 'amd7293@psu.edu' --password 'A09381g@0027' --district '4' --start_year 2021 --end_year 2021 --file_type 'station_5min'
 cd /storage/home/amd7293/caltrans-pems/saved_data/
-mkdir -p dist_$dist
+mkdir -p dist_4
 cd dist_4
-mkdir -p $year
+mkdir -p 2021
 cd /storage/home/amd7293/caltrans-pems/data/
-mv *$dist* /storage/home/amd7293/caltrans-pems/saved_data/dist_$dist/$year
+mv *$dist* /storage/home/amd7293/caltrans-pems/saved_data/dist_4/2021
 rm -rf /storage/home/amd7293/caltrans-pems/data/{*,.*}
