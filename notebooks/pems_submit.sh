03d23eb97aa93c7f36642b623a11b17fd34c29e3
adpsu <64772508+adpsu@users.noreply.github.com>
2023-11-08 03:06:08 -0500

    Update pems_submit.sh

--- notebooks/pems_submit.sh
+++ notebooks/pems_submit.sh
@@ -13,4 +13,4 @@ source ~/.bashrc
 module purge
 module load anaconda3
 conda activate pems
-python3 pems_data_downloader.py --user_id 'amd7293@psu.edu' --password 'A09381g@0027' --district 4 --start_year 2021 --end_year 2021 --file_type 'station_5min'
+python3 pems_data_downloader.py --user_id 'amd7293@psu.edu' --password 'abcd1234' --district 4 --start_year 2021 --end_year 2021 --file_type 'station_5min'
